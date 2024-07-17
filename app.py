bbd7ff031e4dad274ab2bd277229c17cef236c97
aileensugiarto <aileensugiarto07@gmail.com>
2024-07-17 13:46:31 +0800

    commit pertama

---
+++ app.py
@@ -0,0 +1,25 @@
+def luas_segitiga():
+  a = int(input("Masukan alas segitiga: "))
+  t = int(input("Masukan tinggi segitiga: "))
+  luas = a * t / 2
+  print(f"Luas: {luas}")
+
+luas_segitiga()
+
+# Hitung Luas Persegi Panjang
+def luas_persegi_panjang():
+  p = int(input("Masukan panjang persegi: "))
+  l = int(input("Masukan lebar persegi: "))
+  luas = p * l
+  print(f"Luas: {luas}")
+
+luas_persegi_panjang()
+
+# Hitung Luas Persegi Panjang
+def luas_lingkaran():
+  phi = 3.14
+  r = int(input("Masukan Jari-jari: "))
+  luas = phi * r * r
+  print(f"Luas: {luas}")
+
+luas_lingkaran()
